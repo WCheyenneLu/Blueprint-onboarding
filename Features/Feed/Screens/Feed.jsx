71b391d30929d52c6de5b7b5b3ca2f003b354e43
cheye <cheye@MacBook-Pro-8.local>
2024-11-25 12:06:15 -0800

    initial commit

--- Features/Feed/Screens/Feed.jsx
+++ Features/Feed/Screens/Feed.jsx
@@ -1,10 +1,20 @@
 import { Button, Text, View } from "react-native";
 import PropTypes from "prop-types";
 import Post from "../Components/Post";
+import { useState, useEffect } from "react";
+import NewPostForm from "../Components/NewPostForm";
 
 export default function Feed({ navigation }) {
+  const [posts, setPosts] = useState([]);
+  const [newId, setNewId] = useState(0);
   const navigateToLanding = () => {
     navigation.navigate("Landing");
+  };
+  const addNewPost = (newPost) => {
+    const updatedPosts = [...posts];
+    updatedPosts.push({ ...newPost, _id: newId });
+    setPosts(updatedPosts);
+    setNewId((id) => id + 1);
   };
   const GIVEN_POSTS = [
     {
@@ -32,6 +42,7 @@ export default function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Posts</Text>
+      <NewPostForm addNewPost={addNewPost} />
       {GIVEN_POSTS.map((post) => (
         <Post key={post._id} username={post.author} body={post.body} />
       ))}
